45fb8e9225e58e75485a1e93591e80d777e36ef8
Leonardo Freitas <leonardofreitasdev@gmail.com>
2020-08-12 10:58:32 -0300

    orders by user

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -19,10 +19,11 @@ class Order {
 class Orders with ChangeNotifier {
   final String _baseUrl = '${Constants.BASE_API_URL}orders';
   String _token;
+  String _userId;
 
   List<Order> _items = [];
 
-  Orders([this._token, this._items = const []]);
+  Orders([this._token, this._userId, this._items = const []]);
 
   List<Order> get items {
     return [..._items];
@@ -34,7 +35,7 @@ class Orders with ChangeNotifier {
 
   Future<void> loadOrders() async {
     List<Order> loadedItems = [];
-    final response = await http.get("$_baseUrl.json?auth=$_token");
+    final response = await http.get("$_baseUrl/$_userId.json?auth=$_token");
     Map<String, dynamic> data = json.decode(response.body);
 
     if (data != null) {
@@ -65,7 +66,7 @@ class Orders with ChangeNotifier {
     final date = DateTime.now();
 
     final response = await http.post(
-      "$_baseUrl.json",
+      "$_baseUrl/$_userId.json?auth=$_token",
       body: json.encode({
         'total': cart.totalAmount,
         'date': date.toIso8601String(),
