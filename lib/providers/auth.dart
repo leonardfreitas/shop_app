7127a93ed05450d96dacb6019b62e334632386be
Leonardo Freitas <leonardofreitasdev@gmail.com>
2020-08-12 08:42:24 -0300

    auth exceptions

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,14 +1,15 @@
 import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop/exceptions/auth_exception.dart';
 
 class Auth with ChangeNotifier {
-  static const _url =
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC1DpbiqvxHv0t4sm1CbKEYvegxBO8fSak';
-
-  Future<void> signup(String email, String password) async {
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyC1DpbiqvxHv0t4sm1CbKEYvegxBO8fSak';
     final response = await http.post(
-      _url,
+      url,
       body: json.encode({
         "email": email,
         "password": password,
@@ -16,8 +17,19 @@ class Auth with ChangeNotifier {
       }),
     );
 
-    print(json.decode(response.body));
+    final responseBody = json.decode(response.body);
+    if (responseBody['error'] != null) {
+      throw AuthException(responseBody['error']['message']);
+    }
 
     return Future.value();
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
--- lib/widgets/auth_card.dart
+++ lib/widgets/auth_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop/exceptions/auth_exception.dart';
 import 'package:shop/providers/auth.dart';
 import 'package:provider/provider.dart';
 
@@ -19,6 +20,24 @@ class _AuthCardState extends State<AuthCard> {
     'password': '',
   };
 
+  void _showErrorDialog(String msg) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text('Ocorreu um erro'),
+        content: Text(msg),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Fechar'),
+          )
+        ],
+      ),
+    );
+  }
+
   Future<void> _submit() async {
     if (!_form.currentState.validate()) {
       return;
@@ -32,10 +51,22 @@ class _AuthCardState extends State<AuthCard> {
 
     Auth auth = Provider.of(context, listen: false);
 
-    if (_authMode == AuthMode.Login) {
-      // logica de login
-    } else {
-      await auth.signup(_authData["email"], _authData["password"]);
+    try {
+      if (_authMode == AuthMode.Login) {
+        await auth.login(
+          _authData["email"],
+          _authData["password"],
+        );
+      } else {
+        await auth.signup(
+          _authData["email"],
+          _authData["password"],
+        );
+      }
+    } on AuthException catch (error) {
+      _showErrorDialog(error.toString());
+    } catch (error) {
+      _showErrorDialog('Aconteceu um erro inesperado');
     }
 
     setState(() {
